c2363397a34c0c36f11731479f4590bec02eeed7
Paul Maillard <Paul.Maillard@aloatec.com>
2025-06-23 20:40:33 +1200

    Création du HelloWorld

---
+++ HelloWorldCMake/HelloWorldCMake.cpp
@@ -0,0 +1,12 @@
+﻿// HelloWorldCMake.cpp : définit le point d'entrée de l'application.
+//
+
+#include "HelloWorldCMake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello Aloatec" << endl;
+	return 0;
+}
