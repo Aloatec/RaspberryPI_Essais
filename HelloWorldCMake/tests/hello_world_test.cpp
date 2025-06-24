2fcf58935237c32a9dd2c6c609462c8b6d01cb89
Paul Maillard <Paul.Maillard@aloatec.com>
2025-06-24 20:55:20 +1200

    remplacement de catch2 par boost-test

---
+++ HelloWorldCMake/tests/hello_world_test.cpp
@@ -0,0 +1,5 @@
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(AdditionFonctionne) {
+    BOOST_CHECK_EQUAL(2 + 2, 4);
+}
---
+++ HelloWorldCMake/tests/main.cpp
@@ -0,0 +1,3 @@
+#define BOOST_TEST_MODULE AdditionFonctionne
+#include <boost/test/unit_test.hpp> 
+
