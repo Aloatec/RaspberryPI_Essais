204c1793f0f4dc4a3eb8b9f7e9e4ace43de427fb
Paul Maillard <Paul.Maillard@aloatec.com>
2025-07-22 02:59:23 +1200

    ajout d'une fonction pour les copies de dossier

---
+++ ProjetSuperDur/cmake/CopyFolder.cmake
@@ -0,0 +1,8 @@
+﻿function(post_build_copy from_target to_target source_dir dest_subdir)
+  set(dest_dir "$<TARGET_FILE_DIR:${to_target}>/${dest_subdir}")
+
+  add_custom_command(TARGET ${from_target} POST_BUILD
+    COMMAND ${CMAKE_COMMAND} -E make_directory "${dest_dir}"
+    COMMAND ${CMAKE_COMMAND} -E copy_directory "${source_dir}" "${dest_dir}"
+  )
+endfunction()
